c8748f2b13327d3712575eaabbb70f23574932c6
Yash Gupta <yash8740gupta@gmail.com>
2024-06-18 02:45:42 +0530

    adding API and display settings

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '7259a4706emsh5509b05aa85743dp136097jsn3be177de6130',
-		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
+async function getWeather(){
+	const location=document.getElementById('location').value;
+	const apiKey='142dd06955e8e9af9bdd48a0a1015c6a';
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
 
-fetch(url, options)
-.then(response => response.json())
-.then (response => console.log(response))
-.catch(err=>console.error(err));
+	try{
+		const response= await fetch(url);
+		const data= await response.json();
+
+		if(data.cod==200){
+			displayWeather(data);
+		}
+		else{
+			document.getElementById('weather-info').innerHTML=`<p>${data.message}</p>`;
+		}
+	}
+	catch(error){
+		document.getElementById('weather-info').innerHTML=`<p>Failed to fetch Weather. Try again.</p>`;
+	}
+}
+
+function displayWeather(data){
+	const weatherInfo=`
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p><strong>Temperature:</strong> ${data.main.temp} °C</p>
+        <p><strong>Weather:</strong> ${data.weather[0].description}</p>
+        <p><strong>Humidity:</strong> ${data.main.humidity} %</p>
+        <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
+    `;
+
+	document.getElementById('weather-info').innerHTML=weatherInfo;
+}
